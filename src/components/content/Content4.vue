e14231325749ef5b6fc5a8a963953ade3c847525
Koyang <mr.koyangyang@gmail.com>
2021-12-25 13:47:37 +0800

    1.4.2

--- src/components/content/Content4.vue
+++ src/components/content/Content4.vue
@@ -1,8 +1,8 @@
 <template>
-  <p style="line-height: 30px; text-align:left;">&nbsp &nbsp &nbsp &nbsp在本虚拟仿真实验项目中，对学生的考核分为三个层次：
-第一个层次是为了检验学生对于基础理论和基本操作技能的掌握程度，主要根据学生学习的时间和对前期调研的广度进行考核；
-第二层次主要考核学生分析问题和实际的设计能力，以最终设计完成度和表现效果为考核依据；
- 第三个层次主要考察学生的创新能力和评判设计方案的能力，能否有创新点和对自己方案的反思。
+  <p style="line-height: 30px; text-align:left;">&nbsp &nbsp &nbsp &nbsp在本虚拟仿真实验项目中，对学生的考核分为三个层次：</br>
+&nbsp &nbsp &nbsp &nbsp第一个层次是为了检验学生对于基础理论和基本操作技能的掌握程度，主要根据学生学习的时间和对前期调研的广度进行考核；</br>
+&nbsp &nbsp &nbsp &nbsp第二层次主要考核学生分析问题和实际的设计能力，以最终设计完成度和表现效果为考核依据；</br>
+&nbsp &nbsp &nbsp &nbsp第三个层次主要考察学生的创新能力和评判设计方案的能力，能否有创新点和对自己方案的反思。</br>
 <!-- <img src="../../../static/img/table2.png"> -->
 <el-empty image="../../../static/img/table2.png" description="参考" :image-size="800"></el-empty>
 </p>
